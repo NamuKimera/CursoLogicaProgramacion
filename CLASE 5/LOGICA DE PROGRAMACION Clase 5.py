194baaf22fc85854d31f131dea62d7f394a3fcd0
NamuKimeraVT <namuquimera@gmail.com>
2025-09-13 13:59:22 -0300

    Clase 5

---
+++ CLASE 5/LOGICA DE PROGRAMACION Clase 5.py
@@ -0,0 +1,9 @@
+# Logica de Programacion
+
+# Clase 5 (EXPRESIONES LAMBDA)
+
+sumar = lambda x, y: x + y  # Expresión lambda que suma dos números
+restar = lambda a, y: a - y
+
+print(sumar(4, 5))
+print(restar(15, 7))
